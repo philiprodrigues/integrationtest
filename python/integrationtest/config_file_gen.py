c0f982f24de655bbf9d0374b42736e7eab06ac11
Pierre Lasorak <pierre.lasorak@gmail.com>
2023-06-22 15:49:41 +0200

    update the integ tests

--- python/integrationtest/config_file_gen.py
+++ python/integrationtest/config_file_gen.py
@@ -4,6 +4,8 @@ import json
 def get_default_config_dict():
     config_dict = {}
 
+    config_dict["detector"] = {}
+    config_dict["daq_common"] = {}
     config_dict["boot"] = {}
     config_dict["timing"] = {}
     config_dict["readout"] = {}
@@ -19,6 +21,6 @@ def get_default_config_dict():
 
 def write_config(file_name, config_dict):
     with open(file_name, 'w+') as fp:
-        json.dump(config_dict, fp)
+        json.dump(config_dict, fp, indent=4)
         fp.flush()
         fp.close()
--- python/integrationtest/integrationtest_nanorc.py
+++ python/integrationtest/integrationtest_nanorc.py
@@ -71,10 +71,10 @@ def pytest_generate_tests(metafunc):
     # variables from the module (which the user _does_ have access to)
     # and parametrize the fixtures here in pytest_generate_tests,
     # which is run at pytest startup
-    
+
     parametrize_fixture_with_items(metafunc, "create_json_files", "confgen_arguments")
     parametrize_fixture_with_items(metafunc, "run_nanorc", "nanorc_command_list")
-    
+
 
 @pytest.fixture(scope="module")
 def create_json_files(request, tmp_path_factory):
@@ -161,7 +161,7 @@ def create_minimal_json_files(request, tmp_path_factory):
     logfile = tmp_path_factory.getbasetemp() / f"stdouterr_minimal_{request.param_index}.txt"
     dro_map_file = tmp_path_factory.getbasetemp() / f"DROMap.json"
     config_arg = ["--detector-readout-map-file", dro_map_file]
-    
+
     if not file_exists(dro_map_file):
         dro_map_contents = getattr(request.module, "dro_map_contents", "[ ]")
         with open(dro_map_file, 'w+') as f:
@@ -239,8 +239,8 @@ def run_nanorc(request, create_json_files, tmp_path_factory):
         # nothing to do since we've already assigned a default value
         pass
     try:
-        if "op_env" in create_json_files.confgen_config["boot"]:
-            rawdata_filename_prefix = create_json_files.confgen_config["boot"]["op_env"]
+        if "op_env" in create_json_files.confgen_config["detector"]:
+            rawdata_filename_prefix = create_json_files.confgen_config["detector"]["op_env"]
             #print(f"The raw data filename prefix is {rawdata_filename_prefix}")
     except ValueError:
         # nothing to do since we've already assigned a default value
