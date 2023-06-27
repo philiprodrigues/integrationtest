a64c06a271216809fd6200f6524e9d5566ae2093
Eric Flumerfelt <eflumerf@fnal.gov>
2023-06-27 16:33:06 -0400

    Use keys() method

--- python/integrationtest/integrationtest_nanorc.py
+++ python/integrationtest/integrationtest_nanorc.py
@@ -118,7 +118,7 @@ def create_json_files(request, tmp_path_factory):
         conf_dict["boot"]["use_connectivity_service"] = False
         conf_dict["boot"]["start_connectivity_service"] = False
 
-    if not "connectivity_service_port" in conf_dict["boot"]:
+    if not "connectivity_service_port" in conf_dict["boot"].keys():
         conf_dict["boot"]["connectivity_service_port"] = 15000 + random.randrange(100)
     write_config(configfile, conf_dict)
 
